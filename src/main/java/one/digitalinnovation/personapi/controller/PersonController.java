ad49a0dedbbb72dfc0c3b48b3357025c859fb6d3
V2power <vitor5088@gmail.com>
2021-07-21 15:00:19 -0300

    Added MapStruct and Validation API no pom.xml Added exceptions "PersonNotFoundException"

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,31 +1,25 @@
 package one.digitalinnovation.personapi.controller;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
-
-    @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+    private PersonService personService;
 
     @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.create(personDTO);
     }
 }
--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -4,26 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import one.digitalinnovation.personapi.enums.PhoneType;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
+import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PhoneDTO {
+public class PersonDTO {
 
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private PhoneType type;
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
 
     @NotEmpty
-    @Size(min = 13, max = 14)
-    private String number;
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotEmpty
+    private String cpf;
+
+    private String birthDate;
+
+    @NotEmpty
+    @Valid
+    private List<PhoneDTO> phones;
 }
